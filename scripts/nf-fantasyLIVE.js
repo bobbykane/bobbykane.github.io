27860550981cacf019565da2c2230d7b32f50e0d
bobbyjkane <talk@bobbyjkane.com>
2017-06-28 01:01:12 -0400

    everything but right column is linked up

---
+++ scripts/nf-fantasyLIVE.js
@@ -0,0 +1,57 @@
+function removeFadeIn() {
+    setTimeout(function(){
+        $("section.main .plays").removeClass("fadeIn");
+    }, 600);
+}
+
+$("a.Plays-YourLeaguesAll, a.Plays-YourLeaguesSpecific").css("display","none");
+
+$("a.Plays-YourLeaguesNone").click(function(){
+    $("section.main .plays").attr("class", " ").addClass("plays YourLeaguesNone fadeIn");
+    removeFadeIn();
+});
+
+$("a.Plays-YourLeaguesAll").click(function(){
+    $("section.main .plays").attr("class", " ").addClass("plays YourLeaguesAllMatchups fadeIn");
+    removeFadeIn();
+    $("a.Plays-YourLeaguesSpecific").css("display", "block");
+});
+
+$("a.Plays-AllNFL").click(function(){
+    $("section.main .plays").attr("class", " ").addClass("plays AllNFL fadeIn");
+    removeFadeIn();
+});
+
+$("a.Plays-YourLeaguesSpecific").click(function(){
+    $("section.main .plays").attr("class", " "). addClass("plays YourLeaguesSpecific fadeIn");
+});
+
+$("a.ImportLeagues").click(function(){
+    $(".importModal").attr("class", "importModal show default fadeIn");
+    $(".overlay").addClass("show fadeIn");
+});
+
+$(".importModal a.close").click(function(){
+    $(".importModal").removeClass("show default fadeIn");
+    $(".overlay").removeClass("show fadeIn");
+});
+
+$(".importModal a.importYahooButton").click(function(){
+    $(".importModal").attr("class", "importModal show logInYahoo");
+});
+
+var loggedIn = null;
+console.log(loggedIn);
+
+$(".importModal a.finishYahoo").click(function(){
+    $(".importModal").attr("class", "importModal show finishedYahoo");
+    var loggedIn = true;
+
+    if ( loggedIn === true ) {
+        $("section.main .leagues").addClass("loggedIn");
+        $("a.ImportLeagues, a.Plays-YourLeaguesNone").remove();
+        $("section.main .plays").addClass("loggedInAllMatchups");
+        $("a.Plays-YourLeaguesAll").css("display","block");
+
+    }
+});
